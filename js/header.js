a346b6120436bcb86e6f08ab3281fcb0dd0c5a28
Konstantin <96935465+konstantinsenatov@users.noreply.github.com>
2024-10-10 12:53:39 +0400

    .

--- js/header.js
+++ js/header.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
     $("#burderHeader").toggleClass("active");
     $("body").toggleClass("overflow-hidden");
   });
+  $("#headerOptions #burderHeader1").on("click", function () {
+    $("#menu").removeClass("show");
+    $("#burderHeader").removeClass("active");
+
+    $("#headerOptions").removeClass("show");
+    $('body').removeClass("overflow-hidden");
+    $('body').removeClass("reduce-width");
+  });
 });
